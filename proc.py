38500b7e397f6d1c1af16e60d9983205296226f8
steven <stevengriffin323@gmail.com>
2013-12-20 10:02:27 -0500

    ...

--- proc.py
+++ proc.py
@@ -9,7 +9,14 @@ def processQuery(args, cur):
     buildSearchTerm(args)
     cur.execute(selectClause + fromClause + whereClause + orderByClause,
                 args)
-    print(cur.fetchall())
+    for row in cur.fetchall():
+        print '==================='
+        iter = row.__iter__()
+        print 'Package name: ' + iter.next()
+        if args['hide_description'] is not '':
+            print 'Description: ' + iter.next()
+        if args['priority']:
+            print 'Priority: ' + iter.next()
 
 def buildOrderBy(args, cur):
     orderByClause = " ORDER BY " + args['sort=alpha'] + " " + args['asc']
@@ -17,6 +24,10 @@ def buildOrderBy(args, cur):
 
 def buildSelect(args, cur):
     selectClause = "SELECT package.name" + args['hide_description']
+    if args['priority']:
+        selectClause += ', compatibility.priority'
+    if args['depend']:
+        selectClause += ', compatibility.dependencies'
     return selectClause
 
 def buildFrom(select, where, args, cur):
@@ -29,10 +40,17 @@ def buildFrom(select, where, args, cur):
     return fromClause
 
 def buildWhere(args, cur):
+    whereClause = ' WHERE '
     if args['exact']:
-        whereClause = ' WHERE package.name = %(search_term)s'
-        return whereClause
-    return ''
+        whereClause += ' package.name = %(search_term)s AND '
+    if args['priority']:
+        whereClause += ' compatibility.priority = %(priority)s AND '
+    if args['depend']:
+        whereClause += ' %(depend)s = ANY(compatibility.dependencies) AND '
+    if whereClause == ' WHERE ':
+        return ''
+    # Trim last ' AND '.
+    return whereClause[0:-5]
 
 def buildSearchTerm(args):
     term = ''
